fb35aa7f915a6c47df67d4ab0778bb9c071fdd47
AlexeyBorisovich <a.senior@internet.ru>
2024-01-20 23:40:52 +0600

    На языке C# создаем программу вычесления среднего арифметического числа

---
+++ Reshenie/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        double average = CalculateAverage(numbers);
+
+        Console.WriteLine("Среднее арифметическое: " + average);
+
+        Console.ReadKey();
+    }
+    static double CalculateAverage(int[] array)
+    {
+        if (array.Length == 0)
+            return 0;
+
+        int sum = 0;
+
+        foreach (int num in array)
+        {
+            sum += num;
+        }
+
+        return (double)sum / array.Length;
+    }
+}
